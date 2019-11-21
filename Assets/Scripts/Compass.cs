2ac9ab3a6bb810157c28550c1e86ea0c1b2a748d
okamoto hinata <madomagi1013@gmail.com>
2019-11-21 12:49:44 +0900

    課題

---
+++ Assets/Scripts/Compass.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform root;
+
+    [SerializeField]
+    private Text trueHeading;
+
+    void Start()
+    {
+        Input.compass.enabled = true;
+        Input.location.Start();
+    }
+
+    void Update ()
+    {
+        root.rotation = Quaternion.Euler(0, 0, Input.compass.trueHeading);
+        trueHeading.text = ((int)Input.compass.trueHeading) + "°";
+    }
+}
